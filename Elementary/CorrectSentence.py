41d5ef5a12235569536035ad5fddc7735ce8b603
ankiwoong <ankiwoong@gmail.com>
2020-04-10 13:32:23 +0900

    Solve Correct_Sentence

---
+++ Elementary/CorrectSentence.py
@@ -0,0 +1,71 @@
+'''
+Correct Sentence
+
+For the input of your function, you will be given one sentence. You have to return a corrected version, that starts with a capital letter and ends with a period (dot).
+
+Pay attention to the fact that not all of the fixes are necessary. If a sentence already ends with a period (dot), then adding another one will be a mistake.
+
+Input:
+A string.
+
+Output:
+A string.
+
+Example:
+correct_sentence("greetings, friends") == "Greetings, friends."
+correct_sentence("Greetings, friends") == "Greetings, friends."
+correct_sentence("Greetings, friends.") == "Greetings, friends."
+1
+2
+3
+Precondition: No leading and trailing spaces, text contains only spaces, a-z A-Z , and .
+
+def correct_sentence(text: str) -> str:
+    """
+        returns a corrected sentence which starts with a capital letter
+        and ends with a dot.
+    """
+    # your code here
+    return text
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(correct_sentence("greetings, friends"))
+    
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert correct_sentence("greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends.") == "Greetings, friends."
+    assert correct_sentence("hi") == "Hi."
+    assert correct_sentence("welcome to New York") == "Welcome to New York."
+    
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+'''
+
+
+def correct_sentence(text: str):
+    # 입력값에서 첫번째 글자를 대문자로 변환 하고 뒤에 입력값 나머지 부분을 붙인다.
+    # upper : 소문자를 대문자로 변환
+    text = text[0].upper() + text[1:]
+
+    # 마지막에 있는 글자가 .가 없으면 입력값에 .을 붙인다
+    # endswith : 마지막에 있는 글자를 찾는 메소드
+    if not text.endswith('.'):
+        text += '.'
+
+    return text
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(correct_sentence("greetings, friends"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert correct_sentence("greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends.") == "Greetings, friends."
+    assert correct_sentence("hi") == "Hi."
+    assert correct_sentence("welcome to New York") == "Welcome to New York."
+
+    print("Coding complete? Click 'Check' to earn cool rewards!")
