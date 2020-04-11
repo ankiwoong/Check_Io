f1e9f99c8297f5b2b9c3112e8e2d588ebf3dc28f
AnKiWoong <ankiwoong@gmail.com>
2020-04-11 14:13:29 +0900

    Solve Between_Markers

---
+++ Home/BetweenMarkers.py
@@ -0,0 +1,83 @@
+'''
+Between M2arkers
+
+You are given a string and two markers (the initial and final). You have to find a substring enclosed between these two markers. But there are a few important conditions:
+
+The initial and final markers are always different.
+If there is no initial marker, then the first character should be considered the beginning of a string.
+If there is no final marker, then the last character should be considered the ending of a string.
+If the initial and final markers are missing then simply return the whole string.
+If the final marker comes before the initial marker, then return an empty string.
+Input: Three arguments. All of them are strings. The second and third arguments are the initial and final markers.
+
+Output:
+A string.
+
+Example:
+between_markers('What is >apple<', '>', '<') == 'apple'
+between_markers('No[/b] hi', '[b]', '[/b]') == 'No'
+
+How it is used: 
+for parsing texts
+
+Precondition:
+can't be more than one final marker and can't be more than one initial. Marker can't be an empty string
+
+def between_markers(text: str, begin: str, end: str) -> str:
+    """
+        returns substring between two given markers
+    """
+    # your code here
+    return ''
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(between_markers('What is >apple<', '>', '<'))
+
+    # These "asserts" are used for self-checking and not for testing
+    assert between_markers('What is >apple<', '>', '<') == "apple", "One sym"
+    assert between_markers("<head><title>My new site</title></head>",
+                           "<title>", "</title>") == "My new site", "HTML"
+    assert between_markers('No[/b] hi', '[b]', '[/b]') == 'No', 'No opened'
+    assert between_markers('No [b]hi', '[b]', '[/b]') == 'hi', 'No close'
+    assert between_markers('No hi', '[b]', '[/b]') == 'No hi', 'No markers at all'
+    assert between_markers('No <hi>', '>', '<') == '', 'Wrong direction'
+    print('Wow, you are doing pretty good. Time to check it!')
+
+'''
+
+
+def between_markers(text: str, begin: str, end: str):
+    # 시작 텍스트가 문자열에 있으면
+    if begin in text:
+        # 시작 인덱스 번호는 find를 통해 찾은 문자열의 위치 + 시작 텍스트의 원소 갯수
+        begin_index = text.find(begin) + len(begin)
+    # 그 외에 경우에는 시작 번호는 0
+    else:
+        begin_index = 0
+    # 끝 텍스트가 문자열에 있으면
+    if end in text:
+        # find를 통해 찾은 문자열의 위치가 끝 인덱스 번호
+        end_index = text.find(end)
+    # 그 이외에 경우는 문자열의 원소 갯수
+    else:
+        end_index = len(text)
+    # 이렇게 찾아낸 인덱스번호를 통해 슬라이싱 작업
+    return text[begin_index:end_index]
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(between_markers('What is >apple<', '>', '<'))
+
+    # These "asserts" are used for self-checking and not for testing
+    assert between_markers('What is >apple<', '>', '<') == "apple", "One sym"
+    assert between_markers("<head><title>My new site</title></head>",
+                           "<title>", "</title>") == "My new site", "HTML"
+    assert between_markers('No[/b] hi', '[b]', '[/b]') == 'No', 'No opened'
+    assert between_markers('No [b]hi', '[b]', '[/b]') == 'hi', 'No close'
+    assert between_markers(
+        'No hi', '[b]', '[/b]') == 'No hi', 'No markers at all'
+    assert between_markers('No <hi>', '>', '<') == '', 'Wrong direction'
+    print('Wow, you are doing pretty good. Time to check it!')
