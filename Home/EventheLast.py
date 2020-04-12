3960e31d52fb4bfa377c1c1188a30b57a68c4de1
AnKiWoong <ankiwoong@gmail.com>
2020-04-12 12:14:29 +0900

    Solve Even_the_Last

---
+++ Home/EventheLast.py
@@ -0,0 +1,64 @@
+'''
+Even the Last
+
+You are given an array of integers. You should find the sum of the integers with even indexes (0th, 2nd, 4th...). Then multiply this summed number and the final element of the array together. Don't forget that the first element has an index of 0.
+
+For an empty array, the result will always be 0 (zero).
+
+Input:
+A list of integers.
+
+Output:
+The number as an integer.
+
+Example:
+checkio([0, 1, 2, 3, 4, 5]) == 30
+checkio([1, 3, 5]) == 30
+checkio([6]) == 36
+checkio([]) == 0
+
+How it is used: Indexes and slices are important elements of coding. This will come in handy down the road!
+
+Precondition: 0 ≤ len(array) ≤ 20
+all(isinstance(x, int) for x in array)
+all(-100 < x < 100 for x in array)
+
+def checkio(array: list) -> int:
+    """
+        sums even-indexes elements and multiply at the last
+    """
+    return 0
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    print('Example:')
+    print(checkio([0, 1, 2, 3, 4, 5]))
+    
+    assert checkio([0, 1, 2, 3, 4, 5]) == 30, "(0+2+4)*5=30"
+    assert checkio([1, 3, 5]) == 30, "(1+5)*5=30"
+    assert checkio([6]) == 36, "(6)*6=36"
+    assert checkio([]) == 0, "An empty array = 0"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+'''
+
+
+def checkio(array: list) -> int:
+
+    # 리스트의 값을 가지고 판단(있으면 True / 없으면 False)
+    if not array:
+        return 0
+
+    # 리스트의 첫번째 값부터 마지막까지 2 스탭씩 띄어서 더하고 array에 마지막 값이랑 곱함
+    return sum(array[::2]) * array[-1]
+
+
+# These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    print('Example:')
+    print(checkio([0, 1, 2, 3, 4, 5]))
+
+    assert checkio([0, 1, 2, 3, 4, 5]) == 30, "(0+2+4)*5=30"
+    assert checkio([1, 3, 5]) == 30, "(1+5)*5=30"
+    assert checkio([6]) == 36, "(6)*6=36"
+    assert checkio([]) == 0, "An empty array = 0"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
